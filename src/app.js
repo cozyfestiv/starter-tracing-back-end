8a2903362db9e69aba2ba2ffb966cc0f3a0e987a
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-07-12 10:46:42 -0400

    updating package.json

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const articlesRouter = require("./articles/articles.router");
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
+const articlesRouter = require('./articles/articles.router');
+const errorHandler = require('./errors/errorHandler');
+const notFound = require('./errors/notFound');
+const logger = require('./config/logger');
 
 const app = express();
 
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 
-app.use("/articles", articlesRouter);
+app.use('/articles', articlesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
